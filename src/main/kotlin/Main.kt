bef7e8e70907afb5d6a31a458fcd3d492ba9daa0
sakebook <sakebook@gmail.com>
2020-04-24 01:49:46 +0900

    Update main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
+external val exports: dynamic
+
 fun main() {
-    println("Hello Cloud Functions Kotlin/JS!!")
+    exports.helloHttp = { req: dynamic, res: dynamic ->
+        res.send(createMessage(req))
+    }
+}
+
+private fun createMessage(req: dynamic): String {
+    val message = when {
+        req.query.name !== undefined -> escapeHTML(req.query.name)
+        req.body.name !== undefined -> escapeHTML(req.body.name)
+        else -> "World"
+    }
+    return "Hello $message"
 }
